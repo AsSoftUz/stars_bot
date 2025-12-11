7409b5abc254d4efd9825550edb9e36cc488b65e
Ahadov <asadbekahadov00@gmail.com>
2025-12-11 21:30:56 +0500

    premium page started

--- src/pages/premium/premium.jsx
+++ src/pages/premium/premium.jsx
@@ -1,8 +1,66 @@
+import "./premium.css"
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 const Stars = () => {
+
+    const navigate = useNavigate();
+    const [selected, setSelected] = useState("1m");
+
+    const plans = [
+        { id: "1m", label: "1 Month", price: 4.99, perMonth: "USD 4.99 / month" },
+        { id: "3m", label: "3 Months", price: 13.99, perMonth: "USD 4.66 / month", save: "SAVE 5%" },
+        { id: "6m", label: "6 Months", price: 26.99, perMonth: "USD 4.49 / month", save: "SAVE 10%" },
+        { id: "12m", label: "12 Months", price: 39.99, perMonth: "USD 3.33 / month", save: "SAVE 33%" },
+    ];
+
     return (
-        <>
-            <h2>Premium</h2>
-        </>
+        <div className="premium">
+            <nav>
+                <button onClick={() => navigate("/")} className="back-btn"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true"><path d="m12 19-7-7 7-7"></path><path d="M19 12H5"></path></svg></button>
+                <div className="left">
+                    <h2>Telegram Premium</h2>
+                </div>
+            </nav>
+
+            <div className="plans">
+                <h3>Choose a plan</h3>
+                {plans.map((plan) => (
+                    <label
+                        key={plan.id}
+                        className={`glass-card plan ${selected === plan.id ? "active" : ""}`}>
+                        <input
+                            type="radio"
+                            value={plan.id}
+                            checked={selected === plan.id}
+                            onChange={() => setSelected(plan.id)}
+                        />
+
+                        <div className="contents">
+                            <div className="content">
+                                <div className="top">
+                                    <h3>{plan.label}</h3>
+                                    {plan.save && <span className="save">{plan.save}</span>}
+                                </div>
+
+                                <p className="per-month">{plan.perMonth}</p>
+                            </div>
+                            <p className="price">USD {plan.price}</p>
+                        </div>
+                    </label>
+                ))}
+            </div>
+
+            <div className="features">
+                <h3>
+                    Included Features</h3>
+                <p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-check text-purple-500 shrink-0" aria-hidden="true"><circle cx="12" cy="12" r="10"></circle><path d="m9 12 2 2 4-4"></path></svg>
+                    Double Limits
+                </p>
+            </div>
+
+        </div>
     )
 }
 export default Stars;
